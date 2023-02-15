9e3f8264d0b5dbd7e90eeddc9c067f5f16916143
sashank tirumala <stsashank6@gmail.com>
2023-02-14 22:09:45 -0500

    chatbot kinda works

--- app.py
+++ app.py
@@ -1,35 +1,18 @@
-import os
-
-import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
+chat_history = []
 
+@app.route('/', methods=['GET', 'POST'])
+def chatbot():
+    if request.method == 'POST':
+        message = request.form['message']
+        chat_history.append(message)
+        response = "Hello"
+        chat_history.append(response)
+        return render_template('index.html', messages=chat_history)
+    else:
+        return render_template('index.html', messages=chat_history)
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+if __name__ == '__main__':
+    app.run(debug=True)
